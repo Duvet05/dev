96dd19027015a373c204b203776273863e5589d2
Cosmo <johan.amador200@gmail.com>
2025-08-21 15:20:38 -0500

    .eslintrc.json and next.config.ts for build

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // Desactivar ESLint durante el build para Vercel
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
